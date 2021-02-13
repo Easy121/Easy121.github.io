9750777269e2978f5a8ae48407c18ea7b23f7f8a
Sam Guo <18811434432@163.com>
2021-02-13 20:53:15 +0800

    VUE implemented - first trial

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <hello></hello>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import hello from './components/hello.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    hello
   }
 }
 </script>
---
+++ src/components/hello.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="hello">
+		{{ msg }}
+	</div>
+</template>
+
+<script>
+export default {
+	el: '#hello',
+	data: () => {
+		return {
+			msg: "Hello World"
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
