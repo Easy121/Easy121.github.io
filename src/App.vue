54a232ae09fbb048267dfc877df625683e15117a
Sam Guo <18811434432@163.com>
2021-02-14 00:11:00 +0800

    migration from css + html to scss + html + vue - stage 1

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <template>
   <div id="app">
-    <hello></hello>
+    <headrow></headrow>
   </div>
 </template>
 
 <script>
-import hello from './components/hello.vue'
+import headrow from './components/headrow.vue'
 
 export default {
   name: 'App',
   components: {
-    hello
+    headrow
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* font setting template */
+@font-face {
+	font-family: titlefont;
+	src: url('./assets/font/Courgette-Regular.ttf');
+}
+
+/* global variables */
+:root {
+	--main-color1: #004edb;
+	--main-color2: black;
+	--title-ratio: calc(16 / 9);
+	--title-font-size: 6vw;
+	--headrow-height: 60px;
+}
+
+/* normalize setting */
+html {
+	line-height: 1.15;
+	-webkit-text-size-adjust: 100%;
+}
+
+img {
+	border-style: none;
+}
+
+body {
+	margin: 0;
+	padding: 0;
+	background-color: white;
 }
 </style>
---
+++ src/components/headrow.vue
@@ -0,0 +1,79 @@
+<template>
+	<div id="headrow">
+		<div id="headrow-menu">
+			<img id="headrow-menu-icon" src="../assets/img/menu-icon.png" alt="menu-icon">
+		</div>
+		<div id="headrow-explore">Explore</div>
+		<div id="headrow-search">
+			<img id="headrow-search-icon" src="../assets/img/search-icon.png" alt="search-icon">
+		</div>
+		<div id="headrow-transition"></div>
+	</div>
+</template>
+
+<script>
+export default {
+	el: '#headrow'
+}
+</script>
+
+<style lang="scss" scoped>
+// use grid to construct headrow
+#headrow {
+	width: 100%;
+	height: var(--headrow-height);
+
+	display: grid;
+	grid-template-columns: 1fr 9fr 3fr 1fr;
+	grid-template-rows: 5fr 1fr;
+	grid-template-areas: 
+		"headrow-menu . headrow-explore headrow-search"
+		"headrow-transition headrow-transition headrow-transition headrow-transition";
+
+	&-menu {
+		grid-area: headrow-menu;
+		/* background-color: #11EBCD; */
+
+		/* flex is more convenient with images */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		&-icon {
+			width: calc(var(--headrow-height) - 30px);
+		}
+	}
+
+	&-explore {
+		grid-area: headrow-explore;
+		/* background-color: #11EBCD; */
+
+		font-family: titlefont;
+		font-size: calc(var(--headrow-height) - 30px);
+
+		place-self: center center;
+	}
+
+	&-search {
+		grid-area: headrow-search;
+		/* background-color: #11EBCD; */
+
+		/* flex is more convenient with images */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		&-icon {
+			width: calc(var(--headrow-height) - 35px);
+		}
+	}
+
+	&-transition {
+		grid-area: headrow-transition;
+		background: 
+			linear-gradient(black, grey 20%, white 80%);
+		/* background-color: #11EBCD; */
+	}
+}
+</style>
+
