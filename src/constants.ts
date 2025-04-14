5084ab41127da4694cd3fab9e17ba81e8ce7cff7
Zijun Guo <18811434432@163.com>
2025-04-14 18:11:55 +0800

    fix social media bug

--- src/constants.ts
+++ src/constants.ts
@@ -9,30 +9,30 @@ import IconPinterest from "@/assets/icons/IconPinterest.svg";
 import { SITE } from "@/config";
 
 export const SOCIALS = [
-  // {
-  //   name: "Github",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: ` ${SITE.title} on Github`,
-  //   icon: IconGitHub,
-  // },
-  // {
-  //   name: "X",
-  //   href: "https://x.com/username",
-  //   linkTitle: `${SITE.title} on X`,
-  //   icon: IconBrandX,
-  // },
-  // {
-  //   name: "LinkedIn",
-  //   href: "https://www.linkedin.com/in/username/",
-  //   linkTitle: `${SITE.title} on LinkedIn`,
-  //   icon: IconLinkedin,
-  // },
-  // {
-  //   name: "Mail",
-  //   href: "mailto:yourmail@gmail.com",
-  //   linkTitle: `Send an email to ${SITE.title}`,
-  //   icon: IconMail,
-  // },
+  {
+    name: "Github",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: ` ${SITE.title} on Github`,
+    icon: IconGitHub,
+  },
+  {
+    name: "X",
+    href: "https://x.com/username",
+    linkTitle: `${SITE.title} on X`,
+    icon: IconBrandX,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/username/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    icon: IconLinkedin,
+  },
+  {
+    name: "Mail",
+    href: "mailto:yourmail@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    icon: IconMail,
+  },
 ] as const;
 
 export const SHARE_LINKS = [
