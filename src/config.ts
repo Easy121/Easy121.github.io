88954f9e67bc61b42aaa965b07e060bd7cdd7626
Zijun Guo <18811434432@163.com>
2025-04-21 07:55:51 +0800

    test sitemap

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  website: "https://easy121.github.io/", // replace this with your deployed domain
+  author: "Zijun Guo",
+  profile: "https://easy121.github.io/",
+  desc: "Zijun Guo's Blog",
   title: "Zijun Guo's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
@@ -14,7 +14,7 @@ export const SITE = {
   editPost: {
     enabled: false,
     text: "Suggest Changes",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/Easy121/Easy121.github.io/",
   },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
